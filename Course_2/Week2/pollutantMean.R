51e46233a9b902616012f4fd6e0363e8a6ad2e24
Emma <57014399+emmanguyen102@users.noreply.github.com>
2020-10-12 21:49:54 +0300

    Update pollutantMean.R

--- Course_2/Week2/pollutantMean.R
+++ Course_2/Week2/pollutantMean.R
@@ -1,1 +1,23 @@
-
+pollutantmean <- function(directory, pollutant, id=1:332) {
+  
+  ## create an empty vector
+  pollutants <- c()
+  
+  ## get a list of file names
+  filenames <- list.files(directory)
+  
+  ## loop through 332 CSV files in sepcdata folder
+  for (i in id) {
+    filepath = paste(directory, "/", filenames[i], sep="")
+    data = read.csv(filepath, header=TRUE)
+    
+    ## add values of the chosen pollutant into the pollutants vector
+    pollutants <- c(pollutants, data[, pollutant])
+  }
+  
+  ## find the mean, without NA values of pollutants vector
+  pollutant_mean <- mean(pollutants, na.rm=TRUE)
+  
+  ## print the result
+  pollutant_mean
+}
